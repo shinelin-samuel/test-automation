a4c454ad851298ed9fc2c670fb4e7117365bc2fe
Shinelin Samuel <s.shinelin@gmail.com>
2024-10-04 23:21:12 -0400

    removed unused imports

--- spring-boot-tests/src/test/java/com/shin/observability/ObservabilityTest.java
+++ spring-boot-tests/src/test/java/com/shin/observability/ObservabilityTest.java
@@ -6,14 +6,9 @@ import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.actuate.observability.AutoConfigureObservability;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
